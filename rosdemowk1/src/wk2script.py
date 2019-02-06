3317de9adf44750cfbecdf8e66e4e92e2a8a034c
Andrew Smith <andrewsmith96@hotmail.co.uk>
2019-02-06 16:12:17 +0000

    robot *kinda* detects the wall then rotates

--- rosdemowk1/src/wk2script.py
+++ rosdemowk1/src/wk2script.py
@@ -3,27 +3,36 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan 
 
+import math
+
 rospy.init_node("lincoln")
 
 # Our movement variables
 speed = 0.5
 rotation = 0
+
 t = Twist()
 
 def laser_scan_callback(laser_scan_data):
      
     global speed
     global rotation
+    global close_to_object
+
+    laser_scan_center = laser_scan_data.ranges[len(laser_scan_data.ranges) / 2]
+    print laser_scan_center
     
     # Are we close to an object?
-    if laser_scan_data.ranges[len(laser_scan_data.ranges) / 2] < 0.6:
-      
-        # Stop moving the robot #
-        speed = 0
+    if laser_scan_center < 0.8:     
         
         # Rotate the robot #
-        rotation = 0.25
+        rotation = 0.8
+        speed = 0
      
+        
+    elif laser_scan_center > 0.5 or math.isnan(laser_scan_center):
+        rotation = 0
+   
 velocity_publisher = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size = 10)
 laser_scan_subscriber = rospy.Subscriber("/scan", LaserScan, laser_scan_callback)
 
