c17fc1cc1a8eff102fa778c0ee41ecf69da9ec41
Andrew Smith <andrewsmith96@hotmail.co.uk>
2019-02-06 15:29:39 +0000

    refactor

--- rosdemowk1/src/wk2script.py
+++ rosdemowk1/src/wk2script.py
@@ -1,35 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  1 11:46:28 2019
+import rospy
 
-@author: student
-"""
-
-
-
-import rospy as r
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan 
 
-r.init_node("Lincoln")
+rospy.init_node("lincoln")
+
+# Our movement variables
 speed = 0.5
-rot = 0;
+rotation = 0
 t = Twist()
 
-def callback(data):
-    if data.ranges[len(data.ranges) / 2] < 0.6:
-        global speed
+def laser_scan_callback(laser_scan_data):
+     
+    global speed
+    global rotation
+    
+    # Are we close to an object?
+    if laser_scan_data.ranges[len(laser_scan_data.ranges) / 2] < 0.6:
+      
+        # Stop moving the robot #
         speed = 0
         
-        global rot
-        rot = 0.25
+        # Rotate the robot #
+        rotation = 0.25
      
+velocity_publisher = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size = 10)
+laser_scan_subscriber = rospy.Subscriber("/scan", LaserScan, laser_scan_callback)
 
-pub = r.Publisher("/mobile_base/commands/velocity",Twist,queue_size = 10)
-sub_laser = r.Subscriber("/scan", LaserScan, callback)
+# Main Robot loop #
+while not rospy.is_shutdown():
 
-while not r.is_shutdown():
-    print(speed)
+    # Move our robot forward #
     t.linear.x = speed
-    t.angular.z = rot
-    pub.publish(t)
+    
+    # Rotate our robot #
+    t.angular.z = rotation
+    
+    # Actually move the robot #
+    velocity_publisher.publish(t)
